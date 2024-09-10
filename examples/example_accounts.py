dffd2bbbf7a29d3b6257d7faf6f69c2e5f090f67
Jose <japmontufar@gmail.com>
2024-09-10 17:08:14 -0600

    updated accounts example

--- examples/example_accounts.py
+++ examples/example_accounts.py
@@ -1,15 +1,15 @@
 from voipms_api import Accounts
 
 kwargs = {
-    "subaccount": "206996_MyAPI-SubAcc",
-    "password": "42wFhdK95sSz",
+    "username": "MyAPI-SubAcc",
+    "password": "MyExamplePassword",
     "internal_extension": 250,
-    "internal_voicemail": 101,
-    "internal_cnam": "API CNAM new",
+    "internal_voicemail": 1,
+    "internal_cnam": "MY INT CNAM",
     "enable_internal_cnam": 1,
     "internal_extension": 8898,
 }
 
 acc = Accounts()
-new_sub_account = acc.update_subaccount(**kwargs)
+new_sub_account = acc.create_subaccount(**kwargs)
 print(new_sub_account)
